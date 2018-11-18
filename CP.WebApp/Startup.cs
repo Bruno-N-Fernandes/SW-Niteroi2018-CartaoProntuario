912108a7b26dc6e22699b206150cee7d42df0499
Bruno N. Fernandes <provider.bnogueira@terceiros.mongeralaegon.com.br>
2018-11-17 23:03:39 -0200

    Commit Inicial

---
+++ CP.WebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CP.WebApp.Startup))]
+namespace CP.WebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
